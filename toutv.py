196704c70944b17eb15a1f2c9c53723dac289b2f
Benjamin Vanheuverzwijn <bvanheu@gmail.com>
2012-01-25 20:35:32 -0500

    1/ Thanks to Marc-Etienne

--- toutv.py
+++ toutv.py
@@ -3,6 +3,8 @@
 # Copyright (c) 2012, Benjamin Vanheuverzwijn <bvanheu@gmail.com>
 # All rights reserved.
 #
+# Thanks to Marc-Etienne M. Leveille
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #     * Redistributions of source code must retain the above copyright
