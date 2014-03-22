e7796e6a34ca8fe61b6f18bbd95719e2eb495100
Philippe Proulx <eeppeliteloop@gmail.com>
2014-03-21 22:12:31 -0400

    config.py: remove unneeded `TOUTV_WSDL_URL`

--- toutv/config.py
+++ toutv/config.py
@@ -26,7 +26,6 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 USER_AGENT = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7'
-TOUTV_WSDL_URL = 'http://api.tou.tv/v1/TouTVAPIService.svc?wsdl'
 TOUTV_PLAYLIST_URL = 'http://api.radio-canada.ca/validationMedia/v1/Validation.html'
 TOUTV_PLAYLIST_PARAMS = {
     'appCode': 'thePlatform',
