0ab17d874f91aeb2b82a46c5b8d7e1d5f228e5c2
Philippe Proulx <eeppeliteloop@gmail.com>
2014-03-25 19:42:42 -0400

    Qt: forgot about_dialog.py

---
+++ toutvqt/about_dialog.py
@@ -0,0 +1,23 @@
+from pkg_resources import resource_filename
+from PyQt4 import uic
+from PyQt4 import Qt
+
+
+class QTouTvAboutDialog(Qt.QDialog):
+    UI_PATH = resource_filename(__name__, 'dat/about_dialog.ui')
+
+    def __init__(self):
+        super(QTouTvAboutDialog, self).__init__()
+
+        self._setup_ui()
+        #self._setup_signals()
+
+    def _setup_signals(self):
+        self.quit_action.triggered.connect(self._app.closeAllWindows)
+
+    def _setup_ui(self):
+        uic.loadUi(QTouTvAboutDialog.UI_PATH, baseinstance=self)
+
+    def show_move(self, pos):
+        self.move(pos)
+        self.exec()
