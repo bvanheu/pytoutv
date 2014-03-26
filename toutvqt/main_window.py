78db5e25bc0d395aa645fb89ad429045b7736282
Philippe Proulx <eeppeliteloop@gmail.com>
2014-03-26 19:42:50 -0400

    Qt: main_window.py: follow PEP-8

--- toutvqt/main_window.py
+++ toutvqt/main_window.py
@@ -40,10 +40,11 @@ class QTouTvMainWindow(Qt.QMainWindow):
 
     def _setup_infos(self):
         self.infos_frame = QInfosFrame()
-        self.emissions_treeview.emission_selected.connect(self.infos_frame.show_emission)
-        self.emissions_treeview.season_selected.connect(self.infos_frame.show_season)
-        self.emissions_treeview.episode_selected.connect(self.infos_frame.show_episode)
-        self.emissions_treeview.none_selected.connect(self.infos_frame.show_infos_none)
+        treeview = self.emissions_treeview
+        treeview.emission_selected.connect(self.infos_frame.show_emission)
+        treeview.season_selected.connect(self.infos_frame.show_season)
+        treeview.episode_selected.connect(self.infos_frame.show_episode)
+        treeview.none_selected.connect(self.infos_frame.show_infos_none)
 
     @staticmethod
     def _get_icon(name):
