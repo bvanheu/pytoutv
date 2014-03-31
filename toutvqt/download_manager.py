ccfeacb9c1e658672bb60f4880c07f4f7dabb3fe
Philippe Proulx <eeppeliteloop@gmail.com>
2014-03-30 20:48:51 -0400

    Qt: download_manager.py: clean Python

--- toutvqt/download_manager.py
+++ toutvqt/download_manager.py
@@ -3,19 +3,36 @@ import queue
 import logging
 from PyQt4 import Qt
 from PyQt4 import QtCore
-from PyQt4.Qt import QEvent
 
-class QDownloadStartEvent(QEvent):
+
+class _DownloadWork:
+    def __init__(self, episode, bitrate):
+        self._episode = episode
+        self._bitrate = bitrate
+
+    def get_episode(self):
+        return self._episode
+
+    def get_bitrate(self):
+        return self._bitrate
+
+
+class _QDownloadStartEvent(Qt.QEvent):
     """Event sent to download workers to make them initiate a download."""
     def __init__(self, type, work):
-        super(QEvent, self).__init__(type)
-        self.work = work
+        super(Qt.QEvent, self).__init__(type)
+
+        self._work = work
+
+    def get_work(self):
+        return self._work
+
 
 class QDownloadManager(Qt.QObject):
     def __init__(self, nb_threads=5):
         super(QDownloadManager, self).__init__()
 
-        self._download_event_type = QEvent.registerEventType()
+        self._download_event_type = Qt.QEvent.registerEventType()
         self._setup_threads(nb_threads)
 
     def exit(self):
@@ -30,7 +47,7 @@ class QDownloadManager(Qt.QObject):
 
         for i in range(nb_threads):
             thread = Qt.QThread()
-            worker = QDownloadWorker(self._download_event_type, i)
+            worker = _QDownloadWorker(self._download_event_type, i)
             self._threads.append(thread)
             self._workers.append(worker)
             self._available_workers.put(worker)
@@ -50,11 +67,11 @@ class QDownloadManager(Qt.QObject):
             self._available_workers.put(worker)
             return
 
-        ev = QDownloadStartEvent(self._download_event_type, work)
+        ev = _QDownloadStartEvent(self._download_event_type, work)
         Qt.QCoreApplication.postEvent(worker, ev)
 
     def download(self, episode, bitrate):
-        work = (episode, bitrate)
+        work = _DownloadWork(episode, bitrate)
         print('queueing work {}'.format(work))
         self._works.put(work)
         self._do_next_work()
@@ -66,30 +83,30 @@ class QDownloadManager(Qt.QObject):
         self._do_next_work()
 
 
-class QDownloadWorker(Qt.QObject):
+class _QDownloadWorker(Qt.QObject):
     finished = QtCore.pyqtSignal(object)
 
     def __init__(self, download_event_type, i):
-        super(QDownloadWorker, self).__init__()
+        super(_QDownloadWorker, self).__init__()
         self._download_event_type = download_event_type
-        self.i = i
+        self._i = i
 
     def do_work(self, work):
-        episode = work[0]
-        bitrate = work[1]
+        episode = work.get_episode()
+        bitrate = work.get_bitrate()
         print('worker {} "downloading" episode {} at {}'.format(self, episode.Title, work))
         time.sleep(10)
         print('worker {} done "downloading" episode {} at {}'.format(self, episode.Title, work))
         self.finished.emit(work)
 
-    def handle_download_event(self, ev):
-        self.do_work(ev.work)
+    def _handle_download_event(self, ev):
+        self.do_work(ev.get_work())
 
     def customEvent(self, ev):
         if ev.type() == self._download_event_type:
-            self.handle_download_event(ev)
+            self._handle_download_event(ev)
         else:
             logging.error("Shouldn't be here")
 
     def __str__(self):
-        return "<QDownloadWorker #%d>" % (self.i)
+        return '<QDownloadWorker #{}>'.format(self._i)
