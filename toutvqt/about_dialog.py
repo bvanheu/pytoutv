fd303cd97ade699328dc0a6c90c8921590c6b6d2
Philippe Proulx <eeppeliteloop@gmail.com>
2014-04-06 03:17:07 -0400

    Qt: fix about dialog

--- toutvqt/about_dialog.py
+++ toutvqt/about_dialog.py
@@ -31,3 +31,5 @@ class QTouTvAboutDialog(utils.QCommonDialog, utils.QtUiLoad):
         self._load_ui(QTouTvAboutDialog._UI_NAME)
         self._set_contents()
         self.adjustSize()
+        self.setFixedWidth(self.width())
+        self.setFixedHeight(self.height())
