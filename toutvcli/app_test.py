8a5afcfdf8967a28cd6eae95eecee39eca3518ad
Simon Marchi <simon.marchi@polymtl.ca>
2015-04-01 21:54:44 -0400

    Add a bit of validation in the lonely test

--- toutvcli/app_test.py
+++ toutvcli/app_test.py
@@ -1,9 +1,14 @@
+import os
 import unittest
 
 from toutvcli import app
 
 class ToutvCliAppTest(unittest.TestCase):
     def testApp(self):
+        filename = 'Mardi.S2011E01.Mardi.460kbps.ts'
+        expected_size = 25836032
         a = app.App(['fetch', '-q', 'MIN', '2327799176', '2303281531'])
         rc = a.run()
         self.assertEquals(rc, 0)
+        self.assertTrue(os.path.exists(filename))
+        self.assertEquals(os.path.getsize(filename), expected_size)
