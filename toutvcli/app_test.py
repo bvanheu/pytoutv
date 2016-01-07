05ce386fc83e29be58356e77c4bcf6b5c9591c54
Simon Marchi <simon.marchi@polymtl.ca>
2016-01-07 12:19:14 -0500

    Update test to use new episode
    The previous one is not there anymore.

--- toutvcli/app_test.py
+++ toutvcli/app_test.py
@@ -5,9 +5,10 @@ from toutvcli import app
 
 class ToutvCliAppTest(unittest.TestCase):
     def testApp(self):
-        filename = 'Mardi.S2011E01.Mardi.460kbps.ts'
-        expected_size = 25836032
-        a = app.App(['fetch', '-q', 'MIN', '2327799176', '2303281531'])
+        filename = 'Strobosketch.S01E01.Épisode.1.559kbps.ts'
+        expected_size = 6888768
+        args = ['fetch', '-f', '-q', 'MIN', 'Strobosketch', 'S01E01']
+        a = app.App(args)
         rc = a.run()
         self.assertEquals(rc, 0)
         self.assertTrue(os.path.exists(filename))
